f9e0f3b70974f7c8ec30016727270f27161fbf82
faria-karim-porna <faria.porna.08@gmail.com>
2023-04-01 17:05:10 +0000

    Partially Convert To React Typescript

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import Movies from "./Components/Movies/Movies";
+import AddAllData from "./Components/AddAllData/AddAllData";
+import Login from "./Components/Login/Login";
+import PrivateRoute from "./Components/PrivateRoute/PrivateRoute";
+
+export function App(): JSX.Element {
+  return (
+    <> 
+      <Router>
+        <Switch>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <PrivateRoute path="/movies">
+            <Movies></Movies>
+          </PrivateRoute>
+          <Route path="/login">
+            <Login></Login>
+          </Route>
+          <Route path="/addAllData">
+            <AddAllData></AddAllData>
+          </Route>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  );
+}
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,54 +1,72 @@
-import React from 'react';
-import './Home.css';
-import twitter from '../../images/twitter.png';
-import facebook from '../../images/facebook.png';
+import React from "react";
+import "./Home.css";
+import twitter from "../../images/twitter.png";
+import facebook from "../../images/facebook.png";
 import instragram from "../../images/instagram.png";
-import play from '../../images/play.png';
-import { Link } from 'react-router-dom';
+import play from "../../images/play.png";
+import { Link } from "react-router-dom";
 const Home = () => {
-    const email = localStorage.getItem("email");
-    const signOut = ()=> {
-        localStorage.clear();
-        window.location.reload(false);
-      }
-    return (
-        <div>
-            <div className = "home-section">
-                
-                <div className = "align-items-center social-media-icons">
-                    <img src = {facebook} className = "icons mb-4 w-50"/>
-                    <img src = {twitter} className = "icons mb-4"/>
-                    <img src = {instragram} className = "icons"/>
-                </div>
-               
-                <div className = "container">
-                <div className = "d-flex justify-content-between pt-3 pb-3">
-                    <Link to = "/" className = "link"><p className = "site-name">Movie<span className = "site-name-red">flix</span></p></Link>
-                    {!email ? <Link to = "/login" className = "link"><p className = "login-logout-button">Login</p></Link> : <Link to = "/login" className = "link"><p className = "login-logout-button" onClick = {signOut}>Logout</p></Link>}
-                    
-                </div>
-                
-                    <div className = "row">
-                        <div className = "col-lg-6 home-text-section">
-                           <div className = "d-flex align-items-center home-description">
-                                <div>
-                                        <p className = "home-description-title">IT'S A <b>SHOW</b> TIME</p>
-                                        <p className = "home-description-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla bibendum ultrices lorem, at efficitur diam auctor id. Nam scelerisque urna dictum aliquet faucibus. Nulla facilisi. Donec in ligula non elit egestas posuere vitae ac odio. Nullam tincidunt volutpat metus, at accumsan libero ullamcorper vitae. Suspendisse potenti.</p>
-                                        <Link to = "/movies" className = "link">
-                                            <div className = "d-flex home-button-section">
-                                                <img src = {play} className = "home-button mr-3"/>
-                                                <div className = "d-flex align-self-center home-button-text">Let's watch the show</div>
-                                            </div>
-                                        </Link>
-                                </div>
-                           </div>
-                        </div>
-                    </div>
-                </div>
-                <div className = "slider-image"></div>
-            </div>
+  const email = localStorage.getItem("email");
+  const signOut = () => {
+    localStorage.clear();
+    window.location.reload(false);
+  };
+  return (
+    <div>
+      <div className="home-section">
+        <div className="align-items-center social-media-icons">
+          <img src={facebook} className="icons mb-4 w-50" />
+          <img src={twitter} className="icons mb-4" />
+          <img src={instragram} className="icons" />
         </div>
-    );
+
+        <div className="container">
+          <div className="d-flex justify-content-between pt-3 pb-3">
+            <Link to="/" className="link">
+              <p className="site-name">
+                Movie<span className="site-name-red">flix</span>
+              </p>
+            </Link>
+            {!email ? (
+              <Link to="/login" className="link">
+                <p className="login-logout-button">Login</p>
+              </Link>
+            ) : (
+              <Link to="/login" className="link">
+                <p className="login-logout-button" onClick={signOut}>
+                  Logout
+                </p>
+              </Link>
+            )}
+          </div>
+
+          <div className="row">
+            <div className="col-lg-6 home-text-section">
+              <div className="d-flex align-items-center home-description">
+                <div>
+                  <p className="home-description-title">
+                    IT'S A <b>SHOW</b> TIME
+                  </p>
+                  <p className="home-description-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla bibendum ultrices lorem, at efficitur diam auctor id. Nam
+                    scelerisque urna dictum aliquet faucibus. Nulla facilisi. Donec in ligula non elit egestas posuere vitae ac odio. Nullam
+                    tincidunt volutpat metus, at accumsan libero ullamcorper vitae. Suspendisse potenti.
+                  </p>
+                  <Link to="/movies" className="link">
+                    <div className="d-flex home-button-section">
+                      <img src={play} className="home-button mr-3" />
+                      <div className="d-flex align-self-center home-button-text">Let's watch the show</div>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="slider-image"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
