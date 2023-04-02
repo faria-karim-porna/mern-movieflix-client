8a77405326c3aa67432d660c3288844c6657f6aa
faria-karim-porna <faria.porna.08@gmail.com>
2023-04-02 18:40:14 +0000

    Add Modals Component

---
+++ src/Components/modals.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import MoviesModal from "./MoviesModal/MoviesModal";
+
+export function Modals(): JSX.Element {
+  return (
+    <>
+     <MoviesModal /> 
+    </>
+  );
+}
